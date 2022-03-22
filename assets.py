ecf99e6403cb77fc4e16d16f6292ad604fdbb91f
Abdelrahmanrezk <abdelrahmanhamdy33@gmail.com>
2022-03-22 13:45:10 +0200

    test q and a

--- assets.py
+++ assets.py
@@ -1,40 +1,45 @@
 
 import pandas as pd
+from data_preprocess import *
 
 def json_to_dataframe(passage_question_objects): 
     pq_ids       = []
     passages     = []
-    surahs       = []
-    verses      = []
+    # surahs       = []
+    # verses      = []
     questions    = []
     answers     = []
-    start_chars = []
+    # start_chars = []
 
     for obj in passage_question_objects:
         answer_     = []
         start_char_ = []
-        
+        ans_resu = {}
         for answer in obj.answers:
-            answer_.append(answer.text)
+            text = clean_text(answer.text)
+            answer_.append(text)
             start_char_.append(answer.start_char)
 
+        ans_resu['text'] = answer_
+        ans_resu['answer_start'] = start_char_
+
         pq_ids.append(obj.pq_id)
-        passages.append(obj.passage)
-        surahs.append(obj.surah)
-        verses.append(obj.verses)
-        questions.append(obj.question)
-        answers.append(answer_)
-        start_chars.append(start_char_)
+        passages.append(clean_text(obj.passage))
+        # surahs.append(obj.surah)
+        # verses.append(obj.verses)
+        questions.append(clean_text(obj.question))
+        answers.append(ans_resu)
+        # start_chars.append(start_char_)
     print("="*50)
     
     to_dict_ = {
-        "pq_id": pq_ids,
-        "passage": passages,
-        "surah": surahs,
-        "verses": verses,
+        "id": pq_ids,
+        "context": passages,
+        # "surah": surahs,
+        # "verses": verses,
         "question": questions,
-        "answers.text": answers,
-        "answers.start_char": start_chars,
+        "answers": answers,
+        # "answers.start_char": start_chars,
     }
     df_file = pd.DataFrame(to_dict_)
 
